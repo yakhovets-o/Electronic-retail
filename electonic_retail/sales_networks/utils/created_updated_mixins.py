418d0e19cfbf7d689288d45b33bb79470012c1bf
yakhovets-o <yakhovetso@gmail.com>
2025-07-27 14:14:20 +0300

    add class Mata in  mixins

--- electonic_retail/sales_networks/utils/created_updated_mixins.py
+++ electonic_retail/sales_networks/utils/created_updated_mixins.py
@@ -7,3 +7,7 @@ class CreatedUpdatedMixins(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        abstract = True
