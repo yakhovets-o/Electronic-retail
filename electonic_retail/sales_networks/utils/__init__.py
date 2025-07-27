64186b1f2a07169e0f8985d4b30382915d16f535
yakhovets-o <yakhovetso@gmail.com>
2025-07-27 14:45:03 +0300

    Add core utils imports

---
+++ electonic_retail/sales_networks/utils/__init__.py
@@ -0,0 +1,6 @@
+"""Utilities for working with sales networks"""
+
+from sales_networks.utils import network_choices
+from sales_networks.utils import created_updated_mixins
+
+__all__ = ("network_choices", "created_updated_mixins")
