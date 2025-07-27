2efc454f7f0e01aacf6c639d2ecb07886e6f9009
yakhovets-o <yakhovetso@gmail.com>
2025-07-27 14:26:16 +0300

    refactor docstring

--- electonic_retail/sales_networks/utils/created_updated_mixins.py
+++ electonic_retail/sales_networks/utils/created_updated_mixins.py
@@ -3,7 +3,7 @@ from  django.db import models
 
 
 class CreatedUpdatedMixins(models.Model):
-    """mixins add created and updated timestamp fields in model"""
+    """Mixins add created and updated timestamp fields in model"""
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
