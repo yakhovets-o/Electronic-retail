10bdbcd7c19d512366aa811d52a0dea5cc4eed52
yakhovets-o <yakhovetso@gmail.com>
2025-07-27 16:39:47 +0300

    add rerbose name in app config

--- electonic_retail/sales_networks/apps.py
+++ electonic_retail/sales_networks/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SalesNetworksConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "sales_networks"
+    verbose_name = "Торговая сеть"
